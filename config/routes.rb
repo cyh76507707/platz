b64ffe2f5bc594f45c6c6b4bbcc1591f99aada12
Andrew Cho <cyh76507707@gmail.com>
2015-12-15 08:52:04 +0900

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get '/login' => 'admin/sessions#new'
+  get '/logout' => 'admin/sessions#destroy'
+
+  namespace :admin do
+    resources :posts
+    resources :comments, only: [:index, :update, :destroy]
+    resources :tags, except: [:index]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :moderators, only: [:index, :edit, :update]
+  end
+end
