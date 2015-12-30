c4008d6dbbf8fff29d651b171d96c2ce03c40250
Andrew Cho <cyh76507707@gmail.com>
2015-12-30 09:41:44 +0900

    DB Drop

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,35 +12,35 @@ moderator = Moderator.create(
 )
 
 
-30.times do
-    post = Post.create(
-        title: Faker::Lorem.sentence(20),
-        content: Faker::Lorem.paragraph,
-        publish: true,
-        moderator: moderator)
+# 30.times do
+#     post = Post.create(
+#         title: Faker::Lorem.sentence(20),
+#         content: Faker::Lorem.paragraph,
+#         publish: true,
+#         moderator: moderator)
     
-    tag = Tag.create(name: Faker::Lorem.word)
+#     tag = Tag.create(name: Faker::Lorem.word)
     
-    post_tag = PostTag.create(post: post, tag: tag)
+#     post_tag = PostTag.create(post: post, tag: tag)
 
-    visitor = Visitor.create(
-    	fullname: Faker::Name.name,
-    	email: Faker::Internet.email)
+#     visitor = Visitor.create(
+#     	fullname: Faker::Name.name,
+#     	email: Faker::Internet.email)
 
-    comment = Comment.create(
-        message: Faker::Lorem.paragraph,
-        status: [true, false].sample,
-        post: post,
-        visitor: visitor)
+#     comment = Comment.create(
+#         message: Faker::Lorem.paragraph,
+#         status: [true, false].sample,
+#         post: post,
+#         visitor: visitor)
 
-    message = Message.create(
-        content: Faker::Lorem.paragraph,
-        status: [true, false].sample,
-        visitor: visitor)
+#     message = Message.create(
+#         content: Faker::Lorem.paragraph,
+#         status: [true, false].sample,
+#         visitor: visitor)
 
-    notifiable = [visitor, comment].sample
+#     notifiable = [visitor, comment].sample
 
-    notification = Notification.create(
-        notifiable_id: notifiable.id,
-        notifiable_type: notifiable.class.name)
-end
+#     notification = Notification.create(
+#         notifiable_id: notifiable.id,
+#         notifiable_type: notifiable.class.name)
+# end
