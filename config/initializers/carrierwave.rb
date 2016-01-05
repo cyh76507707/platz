66b463f01c7134d07f86dac4c686133cfa4783ad
Andrew Cho <cyh76507707@gmail.com>
2016-01-05 22:07:28 +0900

    Force change AWS settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,12 @@ if Rails.env == 'production'
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => 'us-west-2',
-      :s3_signature_version  => :v4
+      :region                => 'us-west-2'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
+  AWS.config(
+    region: 'us-west-2',
+    s3_signature_version: :v4
+  )
 end
