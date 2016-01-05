edc21d81ed475aa498331eb4dd66faf5c85db238
Andrew Cho <cyh76507707@gmail.com>
2016-01-05 22:11:34 +0900

    Try change S3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ if Rails.env == 'production'
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => 'us-west-2'
+      :region                => 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
   AWS.config(
-    region: 'us-west-2',
+    region: 'us-east-1',
     s3_signature_version: :v4
   )
 end
