402a7c40d260b2dfa8e6ebd3dc89a84d44199c2b
Andrew Cho <cyh76507707@gmail.com>
2016-01-05 21:27:36 +0900

    Change AWS region setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ if Rails.env == 'production'
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => 'eu-west-1'
+      :region                => 'us-west-2'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
