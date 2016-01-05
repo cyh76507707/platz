01c24be24608a9490b5ff552771028c09bc43540
Andrew Cho <cyh76507707@gmail.com>
2016-01-05 22:02:10 +0900

    Add S3 signature version config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ if Rails.env == 'production'
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => 'us-west-2'
+      :region                => 'us-west-2',
+      :s3_signature_version  => :v4
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
